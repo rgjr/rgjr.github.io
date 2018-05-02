4fe8e4d6a740a8be44f2dd30d8fe330e37ee523b
Raul Gutierrez <raul.gutierrez@verys.com>
2018-05-02 11:17:39 -0700

    added ParallaxBackground and ScrollTop components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import LandingPage from './pages/LandingPage';
 import AboutPage from './pages/AboutPage';
 import PortfolioPage from './pages/PortfolioPage';
+import ParallaxBackground from './components/ParallaxBackground';
+import ScrollTop from './components/ScrollTop';
 import ThemeSwitcher from './components/ThemeSwitcher';
 import Browser from './components/Browser';
 
@@ -10,9 +12,13 @@ class App extends Component {
     return (
       <div className="dev-landing-page">
         <ThemeSwitcher>
+          <Browser except firefox>
+            <ParallaxBackground />
+          </Browser>
           <LandingPage />
           <AboutPage />
           <PortfolioPage />
+          <ScrollTop />
         </ThemeSwitcher>
       </div>
     );
